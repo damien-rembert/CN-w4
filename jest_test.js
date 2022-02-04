f0a72aa255ed3c91969c681f63742d42e18aac97
Damien R <damien.rembert@gmail.com>
2022-02-04 16:40:09 +0000

    adding jest examples

--- jest_test.js
+++ jest_test.js
@@ -1,9 +1,34 @@
+const { array } = require("yargs");
+
 const add = (num1, num2) => {
     return (num1 + num2);
 };
-
 // console.log(add(5,6));
+
+
+const subtract = (num1, num2) => {
+    return (num1 - num2);
+};
+
+const multiply = (num1, num2) => {
+    return (num1 * num2);
+};
+
+const divide = (num1, num2) => {
+    return (num1 / num2);
+};
+
+const copyArray = (array) => {
+    return [...array];
+
+}
+
+
 
 module.exports = {
     add,
+    subtract,
+    multiply,
+    divide,
+    copyArray,
 };
--- jest_test.test.js
+++ jest_test.test.js
@@ -1,11 +1,31 @@
 // const {expect} = require("@jest/globals");
 // const {test} = require("jest-circus");
-const {add} = require("./jest_test");
+const { expect } = require("@jest/globals");
+const { notDeepEqual } = require("assert");
+const {add, subtract, multiply, divide, copyArray} = require("./jest_test");
+
+let array = [1, 2, 3];
 
 test("two numbers add up", () => {
     expect(add(1, 2)).toBe(3);
 });
 
+test("two numbers - one minus the other", () => {
+    expect(subtract(3, 2)).toBe(1);
+});
 
+test("two numbers multiplying each other", () => {
+    expect(multiply(5, 2)).toBe(10);
+});
+
+test("two numbers dividing each other", () => {
+    expect(divide(21, 7)).toBe(3);
+});
+
+test("return is a copy", () => {
+    expect(copyArray(array)).not.toBe(array);
+    expect(copyArray(array)).toEqual(array); 
+
+})
 
 
